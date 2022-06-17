ade29c7b3266c8f78fbbe7c127443c25f59ce57a
CodeArtificial <codeaartificial@gmail.com>
2022-06-18 00:47:39 +0700

    Calendar UI Implementation
    index.html:
    - Defer javscript file "script.js" to run after html file elements initalized
    
    script.js
    - Finish rendering the calendar UI by adding the weekdays and padding-days.
    
    style.css:
    - Style the calendar UI with basic border

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let nav = 0;
 
 // Element id that are clicked will be set to click
-let click = null;
+let clickEvent = null;
 
 // Array of events objects 
 let events = localStorage.getItem('events') 
@@ -21,6 +21,30 @@ function render() {
     const day = date.getDate();
     const month = date.getMonth();
     const year = date.getFullYear();
+
+    // Find the first day of month
+    const monthFirstDay = new Date(year, month, 1);
+
+    // Number of days in a month
+    const monthDays = new Date(year, month + 1, 0).getDate();
+
+    // string: "Weekdays, dd/mm/yyyy"
+    const fullDate = monthFirstDay.toLocaleDateString('en-us', {weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric'});
+    console.log(fullDate)
+    // Number of padding days in a month
+    const daysPad = weekdays.indexOf(fullDate.split(', ')[0]);
+
+    for (let i = 1; i <= daysPad + monthDays; i++) {
+        const div = document.createElement('div');
+        div.classList.add('day');
+
+        if (i > daysPad) {
+            div.innerHTML = i - daysPad;
+        } else {
+            div.classList.add('padding-day');
+        }
+        calendar.appendChild(div);
+    }
 }
 
 render();
