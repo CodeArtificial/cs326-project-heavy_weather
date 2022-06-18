87eb75d8b4e4d15ab28add4f38349f530b598579
CodeArtificial <codeaartificial@gmail.com>
2022-06-18 15:02:20 +0700

    Added Form UI
    index.html:
    - Added div elements for form UI
    
    script.js:
    - Added event listener to open form upon clicking on the respective date in the calendar
    
    style.css:
    - Basic form UI appearing

--- script.js
+++ script.js
@@ -14,10 +14,37 @@ let events = localStorage.getItem('events')
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const calendar = document.getElementById('calendar');
+const form = document.getElementById('form');
+const newEvent = document.getElementById('newEvent');
+
+function formOpen(date) {
+    // Set the click event on the date
+    clickEvent = date;
+
+    // Find the event created based on date
+    const event = events.find(e => e.date === clickEvent);
+
+    // If the event already exist in that date
+    if (event) {
+        // Alert the user
+        alert("Event already exist");
+    } else { // Display the event
+        newEvent.style.display = 'block';
+    }
+
+    // Display the form
+    form.style.display = 'block';
+}
 
 // Function to render the page
 function render() {
     const date = new Date();
+
+    if (nav !== 0) {
+        // Set to the new month based on nav
+        date.setMonth(new Date().getMonth() + nav);
+    }
+
     const day = date.getDate();
     const month = date.getMonth();
     const year = date.getFullYear();
@@ -30,16 +57,30 @@ function render() {
 
     // string: "Weekdays, dd/mm/yyyy"
     const fullDate = monthFirstDay.toLocaleDateString('en-us', {weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric'});
-    console.log(fullDate)
+    
     // Number of padding days in a month
     const daysPad = weekdays.indexOf(fullDate.split(', ')[0]);
 
+    // Display the month on the current calendar
+    document.getElementById('monthText').innerText = ` ${date.toLocaleDateString('en-us', {month: 'long'})} ${year}`;
+
+    // Refresh calendar blocks
+    calendar.innerHTML = '';
+
+    // Render the calendar squares
     for (let i = 1; i <= daysPad + monthDays; i++) {
         const div = document.createElement('div');
         div.classList.add('day');
 
+        // Filter padding days from actual day
         if (i > daysPad) {
+            // Add date on block
             div.innerHTML = i - daysPad;
+
+            // Add click event to create new event
+            div.addEventListener('click', () => {
+                formOpen(`${month + 1}/${i - daysPad}/${year}`)
+            });
         } else {
             div.classList.add('padding-day');
         }
@@ -47,4 +88,19 @@ function render() {
     }
 }
 
+function button() {
+    document.getElementById('next').addEventListener('click', () => {
+        // Increment nav to navigate to one month later
+        nav++;
+        render();
+    });
+
+    document.getElementById('back').addEventListener('click', () => {
+        // Decrement nav to navigate to one month earlier 
+        nav--;
+        render();
+    });
+}
+
+button();
 render();
