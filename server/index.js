56c2ec2a04eac1381fecfc99754153d393bebaea
CodeArtificial <codeaartificial@gmail.com>
2022-07-15 00:16:47 +0700

    Added Postgresql to the server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import logger from 'morgan';
-// import { Database } from './database';
+import { Database } from './database';
+
+const DATABASE_URL = "postgres://jegbssbvkhyjdl:0e2c7ce25fa24f96cb65d0b3dc74604f9e0914e975b8bcfa0d6b040a10312ab8@ec2-54-87-179-4.compute-1.amazonaws.com:5432/datvauq86hv760";
 
 class Server {
     constructor() {
-        // this.dburl = dburl;
+        this.dburl = dburl;
         this.app = express();
         this.app.use('/', express.static('client'));
     }
@@ -14,8 +16,8 @@ class Server {
 
         this.app.post('/createEvent', async (request, response) => {
             try {
-                // const { name, date } = request.query;
-                // const event = await self.db.createEvent(name, event);
+                const { name, date } = request.body;
+                await self.db.createEvent(name, date);
                 response.status(200).json({ status: "success" });
             } catch (err) {
                 response.status(404).json({ status: "failed" });
@@ -24,22 +26,22 @@ class Server {
 
         this.app.get('/getAllEvent', async (request, response) => {
             try {
-                // const events = await self.db.readAllEvent();
-                // response.status(200).json(events);
+                const events = await self.db.readAllEvent();
+                response.status(200).json(events);
             } catch (err) {
                 response.status(404).json({ status: "failed" });
             }
         });
     }
 
-    // async initDB() {
-    //     this.db = new Database(this.dburl);
-    //     await this.db.connect();
-    // }
+    async initDB() {
+        this.db = new Database(this.dburl);
+        await this.db.connect();
+    }
 
     async start() {
         await this.initRoutes();
-        // await this.initDB();
+        await this.initDB();
         app.use(express.json());
         app.use(express.urlencoded({ extended: false }));
         app.use(logger('dev'));
@@ -51,25 +53,5 @@ class Server {
     }
 }
 
-// const server = new Server();
-// server.start();
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-// TODO #3: Add middleware to the Express app.
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(logger('dev'));
-app.use('/', express.static('client'));
-
-// TODO #4: Implement the /wordScore endpoint
-app.post('/createEvent', async (request, response) => {
-//   const { name, word, score } = request.body;
-//   await database.saveWordScore(name, word, score);
-  response.status(200).json({ status: 'success' });
-});
-
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+const server = new Server(DATABASE_URL);
+server.start();
